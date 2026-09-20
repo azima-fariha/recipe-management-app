d8d3ddd1e8927a129102341ca93c06b69914614b
azima-fariha <fariha9292@gmail.com>
2026-09-20 14:52:36 +0200

    Make full_name required in user schema

--- user-service/schemas.py
+++ user-service/schemas.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, EmailStr
 
 class UserDto(BaseModel):
     id: Optional[int] = None
-    full_name: Optional[str] = None
+    full_name: str
     email: EmailStr
     dietary_restrictions: Optional[str] = None
     preferences: Optional[str] = None
